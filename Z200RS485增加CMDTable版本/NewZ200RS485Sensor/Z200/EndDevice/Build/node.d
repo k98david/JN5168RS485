1e6235c97423af4ffa12caa1d0d7a8fac4c20ba3
k98david <k98david@gmail.com>
2018-03-21 10:27:13 +0800

    增加Flash CMD List還沒有增加

---
+++ Z200RS485增加CMDTable版本/NewZ200RS485Sensor/Z200/EndDevice/Build/node.d
@@ -0,0 +1,119 @@
+node.o: \
+ c:/nxp/bstudio_nxp/workspace/802-15-4-Cable-Replace/Common/Source/node.c \
+ c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/../../../../ba-elf/include/string.h \
+ c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/../../../../ba-elf/include/_ansi.h \
+ c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/../../../../ba-elf/include/newlib.h \
+ c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/../../../../ba-elf/include/sys/config.h \
+ c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/../../../../ba-elf/include/machine/ieeefp.h \
+ c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/../../../../ba-elf/include/sys/features.h \
+ c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/../../../../ba-elf/include/sys/reent.h \
+ c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/../../../../ba-elf/include/_ansi.h \
+ c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/../../../../ba-elf/include/sys/_types.h \
+ c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/../../../../ba-elf/include/machine/_types.h \
+ c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/../../../../ba-elf/include/machine/_default_types.h \
+ c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/../../../../ba-elf/include/sys/lock.h \
+ c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/include/stddef.h \
+ c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/../../../../ba-elf/include/sys/cdefs.h \
+ c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/../../../../ba-elf/include/sys/string.h \
+ c:/nxp/bstudio_nxp/sdk/JN-SW-4163/Components/AppApi/Include/AppApi.h \
+ c:/nxp/bstudio_nxp/sdk/JN-SW-4163/Components/AppApi/Include/AppApi_JN5168.h \
+ c:/nxp/bstudio_nxp/sdk/JN-SW-4163/Components/AppApi/Include/AppApi_JN516x.h \
+ c:/nxp/bstudio_nxp/sdk/JN-SW-4163/Components/Common/Include/jendefs.h \
+ c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/include/stdint.h \
+ c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/../../../../ba-elf/include/stdint.h \
+ c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/include/stdarg.h \
+ c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/../../../../ba-elf/include/stdio.h \
+ c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/../../../../ba-elf/include/sys/types.h \
+ c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/../../../../ba-elf/include/machine/types.h \
+ c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/../../../../ba-elf/include/sys/stdio.h \
+ c:/nxp/bstudio_nxp/sdk/JN-SW-4163/Components/MAC/Include/mac_sap.h \
+ c:/nxp/bstudio_nxp/sdk/JN-SW-4163/Components/MAC/Include/mac_sap_CombinedMac.h \
+ c:/nxp/bstudio_nxp/sdk/JN-SW-4163/Components/MAC/Include/mac_pib.h \
+ c:/nxp/bstudio_nxp/sdk/JN-SW-4163/Components/MAC/Include/mac_pib_CombinedMac.h \
+ c:/nxp/bstudio_nxp/sdk/JN-SW-4163/Components/MAC/Include/mac_config.h \
+ c:/nxp/bstudio_nxp/sdk/JN-SW-4163/Components/MAC/Include/mac_sap.h \
+ c:/nxp/bstudio_nxp/sdk/JN-SW-4163/Platform/DK4/Include/Button.h \
+ c:/nxp/bstudio_nxp/sdk/JN-SW-4163/Components/HardwareApi/Include/AppHardwareApi.h \
+ c:/nxp/bstudio_nxp/sdk/JN-SW-4163/Components/HardwareApi/Include/AppHardwareApi_JN5168.h \
+ c:/nxp/bstudio_nxp/sdk/JN-SW-4163/Components/HardwareApi/Include/AppHardwareApi_JN516x.h \
+ c:/nxp/bstudio_nxp/sdk/JN-SW-4163/Platform/DK4/Include/PlatformDIOAssignments.h \
+ c:/nxp/bstudio_nxp/workspace/802-15-4-Cable-Replace/Common/Source/dbg.h \
+ c:/nxp/bstudio_nxp/workspace/802-15-4-Cable-Replace/Common/Source/node.h
+
+c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/../../../../ba-elf/include/string.h:
+
+c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/../../../../ba-elf/include/_ansi.h:
+
+c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/../../../../ba-elf/include/newlib.h:
+
+c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/../../../../ba-elf/include/sys/config.h:
+
+c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/../../../../ba-elf/include/machine/ieeefp.h:
+
+c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/../../../../ba-elf/include/sys/features.h:
+
+c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/../../../../ba-elf/include/sys/reent.h:
+
+c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/../../../../ba-elf/include/_ansi.h:
+
+c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/../../../../ba-elf/include/sys/_types.h:
+
+c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/../../../../ba-elf/include/machine/_types.h:
+
+c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/../../../../ba-elf/include/machine/_default_types.h:
+
+c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/../../../../ba-elf/include/sys/lock.h:
+
+c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/include/stddef.h:
+
+c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/../../../../ba-elf/include/sys/cdefs.h:
+
+c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/../../../../ba-elf/include/sys/string.h:
+
+c:/nxp/bstudio_nxp/sdk/JN-SW-4163/Components/AppApi/Include/AppApi.h:
+
+c:/nxp/bstudio_nxp/sdk/JN-SW-4163/Components/AppApi/Include/AppApi_JN5168.h:
+
+c:/nxp/bstudio_nxp/sdk/JN-SW-4163/Components/AppApi/Include/AppApi_JN516x.h:
+
+c:/nxp/bstudio_nxp/sdk/JN-SW-4163/Components/Common/Include/jendefs.h:
+
+c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/include/stdint.h:
+
+c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/../../../../ba-elf/include/stdint.h:
+
+c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/include/stdarg.h:
+
+c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/../../../../ba-elf/include/stdio.h:
+
+c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/../../../../ba-elf/include/sys/types.h:
+
+c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/../../../../ba-elf/include/machine/types.h:
+
+c:\nxp\bstudio_nxp\sdk\tools\ba-elf-ba2-r36379\bin\../lib/gcc/ba-elf/4.7.4/../../../../ba-elf/include/sys/stdio.h:
+
+c:/nxp/bstudio_nxp/sdk/JN-SW-4163/Components/MAC/Include/mac_sap.h:
+
+c:/nxp/bstudio_nxp/sdk/JN-SW-4163/Components/MAC/Include/mac_sap_CombinedMac.h:
+
+c:/nxp/bstudio_nxp/sdk/JN-SW-4163/Components/MAC/Include/mac_pib.h:
+
+c:/nxp/bstudio_nxp/sdk/JN-SW-4163/Components/MAC/Include/mac_pib_CombinedMac.h:
+
+c:/nxp/bstudio_nxp/sdk/JN-SW-4163/Components/MAC/Include/mac_config.h:
+
+c:/nxp/bstudio_nxp/sdk/JN-SW-4163/Components/MAC/Include/mac_sap.h:
+
+c:/nxp/bstudio_nxp/sdk/JN-SW-4163/Platform/DK4/Include/Button.h:
+
+c:/nxp/bstudio_nxp/sdk/JN-SW-4163/Components/HardwareApi/Include/AppHardwareApi.h:
+
+c:/nxp/bstudio_nxp/sdk/JN-SW-4163/Components/HardwareApi/Include/AppHardwareApi_JN5168.h:
+
+c:/nxp/bstudio_nxp/sdk/JN-SW-4163/Components/HardwareApi/Include/AppHardwareApi_JN516x.h:
+
+c:/nxp/bstudio_nxp/sdk/JN-SW-4163/Platform/DK4/Include/PlatformDIOAssignments.h:
+
+c:/nxp/bstudio_nxp/workspace/802-15-4-Cable-Replace/Common/Source/dbg.h:
+
+c:/nxp/bstudio_nxp/workspace/802-15-4-Cable-Replace/Common/Source/node.h:
